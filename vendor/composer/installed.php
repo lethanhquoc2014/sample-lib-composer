cfcd338f920053bc3d7865bc6f7a0112b8b053c3
Le Thanh Quoc <lethanhquoc@AIO0103.local>
2022-08-26 09:52:20 +0700

    build sample lib composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '3c8e09533b5335a327ffc49acf278b6e6fcd94cd',
+    'name' => 'quoclt/sample-lib-composer',
+  ),
+  'versions' => 
+  array (
+    'monolog/monolog' => 
+    array (
+      'pretty_version' => '1.0.2',
+      'version' => '1.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b704c49a3051536f67f2d39f13568f74615b9922',
+    ),
+    'quoclt/sample-lib-composer' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3c8e09533b5335a327ffc49acf278b6e6fcd94cd',
+    ),
+  ),
+);
